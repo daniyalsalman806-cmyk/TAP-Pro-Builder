a856ccf48d2a9a87bbac70b9fbef8afc0533c7db
daniyalsalman806-cmyk <daniyalsalman806@gmail.com>
2026-05-03 01:12:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if "GOOGLE_API_KEY" not in st.secrets:
 else:
     try:
         genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-        model = genai.GenerativeModel('gemini-1.5-flash')
+     model = genai.GenerativeModel('gemini-pro')
     except Exception as e:
         st.error(f"Config Error: {e}")
 
